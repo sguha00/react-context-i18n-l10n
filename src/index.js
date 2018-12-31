c4c27ed376ffef4d2268c8ce0f3de73ce3d022cd
Shayan Guha <shayanguha@gmail.com>
2018-12-31 11:39:47 -0800

    Refactor

--- src/index.js
+++ src/index.js
@@ -3,18 +3,8 @@ import { render } from "react-dom";
 import Localized from "./Localized";
 import Toggle from "./Toggle";
 
-// import localization libraries
-import moment from "moment";
-import numeral from "numeral";
-import i18next from 'i18next';
-import "numeral/locales";
-import "moment/locale/es";
-import "moment/locale/de";
-import "moment/locale/fr";
-import "moment/locale/it";
-
 // import context provider
-import { ContextProvider } from "./localizationContext";
+import { LocaleProvider } from "./localizationContext";
 
 const time = 1528206960000;
 const data = { amount: 19879989776, fraction: 0.225 };
@@ -24,71 +14,14 @@ const data = { amount: 19879989776, fraction: 0.225 };
 
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      lang: 'es',
-    }
-    i18next.init({
-      lng: this.state.lang,
-      resources: {
-        en: {
-          translation: {
-            "date": "Date",
-            "amount": "Amount",
-            "percent": "Percent",
-          }
-        },
-        es: {
-          translation: {
-            "date": "Fecha",
-            "amount": "Cantidad",
-            "percent": "Por Ciento",
-          }
-        }
-    
-      }
-    });
-  }
-
-  localizedMoment = (...args) => {
-    return moment(...args).locale(this.state.lang);
-  };
-
-  localizedNumeral = (...args) => {
-    numeral.locale(this.state.lang);
-    return numeral(...args);
-  };
-
-  translate = (...args) => {
-    return i18next.t(args);
-  };
-
-  toggleLocale = () => {
-    let lang;
-    if (this.state.lang === 'en') {
-      lang = 'es';
-    } else {
-      lang = 'en';
-    }
-    this.setState({lang});
-    i18next.changeLanguage(lang);
-  }
-
   render() {
     return (
-      <ContextProvider 
-        moment={this.localizedMoment}
-        numeral={this.localizedNumeral}
-        translate={this.translate}
-        toggleLocale={this.toggleLocale}
-      >
+      <LocaleProvider>
         <Toggle />
         <div style={{ fontFamily: "Helvetica" }}>
           <Localized data={data} time={time} />
         </div>
-      </ContextProvider>
+      </LocaleProvider>
     );
   }
 }
--- src/localizationContext.js
+++ src/localizationContext.js
@@ -1,21 +1,74 @@
 import React from "react";
+import moment from "moment";
+import numeral from "numeral";
+import i18next from 'i18next';
+import "numeral/locales";
+import "moment/locale/es";
+import "moment/locale/de";
+import "moment/locale/fr";
+import "moment/locale/it";
+
+import * as en from "./i18n/en.json";
+import * as es from "./i18n/es.json";
 
 const Context = React.createContext();
 
 // create Context.Provider : the value prop is our toolbox
-export const ContextProvider = props => {
-  return (
-    <Context.Provider
-      value={{
-        moment: props.moment,
-        numeral: props.numeral,
-        translate: props.translate,
-        toggleLocale: props.toggleLocale,
-      }}
-    >
-      {props.children}
-    </Context.Provider>
-  );
+export class LocaleProvider extends React.Component {
+  constructor() {
+    super();
+    debugger;
+    this.state = {
+      lang: 'es',
+    }
+    console.log(es, en);
+    i18next.init({
+      lng: this.state.lang,
+      resources: {
+        ...en,
+        ...es,
+      }
+    });
+  }
+
+  localizedMoment = (...args) => {
+    return moment(...args).locale(this.state.lang);
+  };
+
+  localizedNumeral = (...args) => {
+    numeral.locale(this.state.lang);
+    return numeral(...args);
+  };
+
+  translate = (...args) => {
+    return i18next.t(args);
+  };
+
+  toggleLocale = () => {
+    let lang;
+    if (this.state.lang === 'en') {
+      lang = 'es';
+    } else {
+      lang = 'en';
+    }
+    this.setState({lang});
+    i18next.changeLanguage(lang);
+  }
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          moment: this.localizedMoment,
+          numeral: this.localizedNumeral,
+          translate: this.translate,
+          toggleLocale: this.toggleLocale,
+        }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 };
 
 // a higher order component to save us the hassle of calling Context.Consumer
